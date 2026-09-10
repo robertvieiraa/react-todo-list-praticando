bb15a37f27589723921af0ada2de4fe56c0df3d6
Robert Vieira <robertvieirasa1987@gmail.com>
2026-09-09 21:41:42 -0300

    Implementei funcionalidade de criar itens

--- src/App.jsx
+++ src/App.jsx
@@ -12,57 +12,81 @@ import { ToDoItem } from "./components/ToDoItem"
 import { ToDoList } from "./components/ToDoList"
 import { TodoForm } from "./components/TodoForm"
 
-const todos = [
-  {
-    id: 1,
-    description: "JSX e componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 2,
-    description: "Props, state e hooks",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 3,
-    description: "Ciclo de vida dos componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 4,
-    description: "Testes unitários com Jest",
-    completed: false,
-    createdAt: "2022-10-31"
-  }
-]
+// const todos = [
+//   {
+//     id: 1,
+//     description: "JSX e componentes",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 2,
+//     description: "Props, state e hooks",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 3,
+//     description: "Ciclo de vida dos componentes",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 4,
+//     description: "Testes unitários com Jest",
+//     completed: false,
+//     createdAt: "2022-10-31"
+//   }
+// ]
 
-const completed = [
-  {
-    id: 5,
-    description: "Controle de inputs e formulários controlados",
-    completed: true,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 6,
-    description: "Rotas dinâmicas",
-    completed: true,
-    createdAt: "2022-10-31"
-  }
-]
+// const completed = [
+//   {
+//     id: 5,
+//     description: "Controle de inputs e formulários controlados",
+//     completed: true,
+//     createdAt: "2022-10-31"
+//   },
+//   {
+//     id: 6,
+//     description: "Rotas dinâmicas",
+//     completed: true,
+//     createdAt: "2022-10-31"
+//   }
+// ]
 
 function App() {
   const [showDialog, setShowDialog] = useState(false)
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      description: "JSX e componentes",
+      completed: false,
+      createdAt: "2022-10-31"
+    },
+    {
+      id: 2,
+      description: "Controle de inputs e formulários controlados",
+      completed: true,
+      createdAt: "2022-10-31"
+    }
+  ])
 
   const toggleDialog = () => {
     setShowDialog(!showDialog)
   }
 
-  const addTodo = () => {
-    console.log('Precisamos adicionar um novo To Do')
+  const addTodo = (formData) => {
+    const description = formData.get('description')
+    setTodos(prevState => {
+      const todo = {
+        id: prevState.length + 1,
+        description,
+        completed: false,
+        createdAt: new Date().toISOString()
+      }
+      return [...prevState, todo]
+    })
     toggleDialog()
   }
 
@@ -77,13 +101,13 @@ function App() {
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
-            {todos.map(function (t) {
+            {todos.filter(t => !t.completed).map(function (t) {
               return <ToDoItem key={t.id} item={t} />
             })}
           </ToDoList>
           <SubHeading>Concluído</SubHeading>
           <ToDoList>
-            {completed.map(function (t) {
+            {todos.filter(t => t.completed).map(function (t) {
               return <ToDoItem key={t.id} item={t} />
             })}
           </ToDoList>
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -7,6 +7,7 @@ export function TodoForm({ onSubmit }) {
         <form action={onSubmit} className='todo-form'>
             <TextInput 
             placeholder="Digite o item que deseja adicionar" 
+            name="description"
             required
             />
             <Button>Salvar item</Button>
