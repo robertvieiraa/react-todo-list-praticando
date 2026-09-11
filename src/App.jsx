211f9f25b55c104c606b9b636d090d98316192af
Robert Vieira <robertvieirasa1987@gmail.com>
2026-09-11 14:48:16 -0300

    Implementei funcionalidade de alternar status do item

--- src/App.jsx
+++ src/App.jsx
@@ -90,6 +90,20 @@ function App() {
     toggleDialog()
   }
 
+  const toggleTodoCompleted = (todo) => {
+    setTodos(prevState => {
+      return prevState.map(t => {
+        if (t.id == todo.id) {
+          return {
+            ...t,
+            completed: !t.completed
+          }
+        }
+        return t
+      })
+    })
+  }
+
   return (
     <main>
       <Container>
@@ -102,13 +116,21 @@ function App() {
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
             {todos.filter(t => !t.completed).map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+              return <ToDoItem 
+              key={t.id} 
+              item={t} 
+              onToggleCompleted={toggleTodoCompleted}
+              />
             })}
           </ToDoList>
           <SubHeading>Concluído</SubHeading>
           <ToDoList>
             {todos.filter(t => t.completed).map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+              return <ToDoItem 
+              key={t.id} 
+              item={t} 
+              onToggleCompleted={toggleTodoCompleted}
+              />
             })}
           </ToDoList>
           <Footer>
