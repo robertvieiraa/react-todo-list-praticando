f0ecb54c2ec985998a8729a791b0769c7dbf69d2
Robert Vieira <robertvieirasa1987@gmail.com>
2026-09-23 20:27:19 -0300

    Adicionei empty state

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { IconPlus, IconSchool } from "./components/icons"
 import { TodoForm } from "./components/TodoForm"
 import TodoContext from "./components/TodoProvider/TodoContext"
 import { TodoGroup } from "./components/TodoGroup"
+import { EmptyState } from "./components/EmptyState"
 
 function App() {
   const { todos, addTodo, openFormTodoDialog, closeFormTodoDialog, showDialog, selectedTodo, editTodo } = use(TodoContext)
@@ -36,6 +37,7 @@ function App() {
             heading="Para estudar"
             items={todos.filter(t => !t.completed)}
           />
+          {todos.length == 0 && <EmptyState/>}
           <TodoGroup
             heading="Concluído"
             items={todos.filter(t => t.completed)}
